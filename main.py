7cb55185e4f62bfd5ee1c419f784ae139c748704
Aryan Mahindra <62005619+Aryan181@users.noreply.github.com>
2025-05-18 13:46:50 -0700

    Q K V matrix

--- main.py
+++ main.py
@@ -6,17 +6,33 @@ sentence = "the dog sat"
 words = sentence.split()  
 final_embeddings = []
 
+queries = []
+keys = []
+values = []
+
+
 for i in range(len(words)):
     vector_4d = np.random.rand(4)  
     
     pos_vector = np.array([np.sin(i), np.cos(i), np.sin(i), np.cos(i)])
 
     combined_vectors = vector_4d + pos_vector
-    
-    word_embeddings.append(word_embeddings)
+
+    word_embeddings.append(vector_4d)
     final_embeddings.append(combined_vectors)
 
 
 matrix = np.array(final_embeddings)
+
+
+W_Q = np.random.rand(4, 4)
+W_K = np.random.rand(4, 4)
+W_V = np.random.rand(4, 4)
+
+
+for embedding in final_embeddings: 
+    queries.append(embedding @ W_Q)
+    keys.append(embedding @ W_K)
+    values.append(embedding @ W_V)
 
 print(matrix)
